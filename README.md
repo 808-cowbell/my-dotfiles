30329208cb35fdfae161f309b976e4e03a91028d
808-cowbell <digitalm34@gmail.com>
2020-05-14 09:44:16 -0600

    added new screenshot, added comments to tile-wallpaper.sh

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # transients
-![Screenshot](/Screenshots/screenshot5.png)
+![Screenshot](/Screenshots/screenshot6.png)
 
 # dependencies
 ### i3 config
@@ -8,7 +8,7 @@ To use the i3 dotfiles properly, you will need to have the following installed:
 - brightnessctl
 - dmenu
 - fish
-- i3-gaps-rounded-git
+- i3-gaps
 - NetworkManager
 - nm-applet
 - pamixer
@@ -35,5 +35,7 @@ To use the i3 dotfiles properly, you will need to have the following installed:
 ``.*rc`` files go in $HOME.
 
 ``xorg.conf.d`` goes to ``/etc/X11/``.
+
+``tile-wallpaper.sh`` can be placed anywhere, but make sure that wpg is configured to not set a wallpaper and call ``tile-wallpaper.sh`` from the directory it's placed in.
 
 Other files and folders go to ``~/.config/``.
--- tile-wallpaper.sh
+++ tile-wallpaper.sh
@@ -1,5 +1,27 @@
 #!/bin/bash
-if (( $(identify -format "%w" "$(< "${HOME}/.cache/wal/wal")") < 1920 )) && (( $(identify -format "%h" "$(< "${HOME}/.cache/wal/wal")") < 1080 ))
+# This script acts as a wallpaper setter for wpgtk that automatically tiles
+# wallpapers under the resolution specified using the 'w' and 'h' variables.
+# Change these variables to your liking.
+#
+# To use this script with wpgtk, you will need to prevent it from setting
+# wallpapers and you will need to configure it to run a command after a
+# theme is set. To achieve this using the wpgtk GUI:
+# - In the "Options" tab, disable the "Set wallpaper" option.
+# - Enable the "Run command after" option.
+# - In the "Command" box, enter "bash /path/to/tile-wallpaper.sh".
+#
+# To restore wallpapers on startup, add tile-wallpaper.sh to the autostart
+# of your window manager after initializing wpgtk. I achieve the latter by 
+# using the "wp-init.sh" file in wpg's "~/.config" directory in my xinitrc.
+# At the moment, I'm not sure of a better way to do this.
+
+# Monitor width
+w=1920
+
+# Monitor height
+h=1080
+
+if (( $(identify -format "%w" "$(< "${HOME}/.cache/wal/wal")") < $w )) && (( $(identify -format "%h" "$(< "${HOME}/.cache/wal/wal")") < $h ))
 then
 	feh --bg-tile "$(< "${HOME}/.cache/wal/wal")"
 else
