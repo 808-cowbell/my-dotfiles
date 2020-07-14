ce7fedee38ec1301c4d2d477306962069d993e1e
808-cowbell <digitalm34@gmail.com>
2020-07-14 11:18:13 -0600

    minor adjustments to configs. stopped using compositor

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -7,6 +7,6 @@ killall -q polybar
 
 # Launch bar1 and bar2
 echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
-polybar wpgtk >>/tmp/polybar1.log 2>&1 &
+polybar -r plantbar >>/tmp/polybar1.log 2>&1 &
 
 echo "Bars launched..."
