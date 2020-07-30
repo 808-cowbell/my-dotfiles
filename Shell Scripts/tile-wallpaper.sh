dea5b8fc7e7410403f789fc7a9a708ce0841a284
808-cowbell <digitalm34@gmail.com>
2020-07-29 22:22:40 -0600

    tile-wallpaper.sh now detects monitor resolution using xrandr

--- Shell Scripts/tile-wallpaper.sh
+++ Shell Scripts/tile-wallpaper.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 # This script acts as a wallpaper setter for wpgtk that automatically tiles
-# wallpapers under the resolution specified using the 'w' and 'h' variables.
-# Change these variables to your liking.
+# wallpapers with a resolution lower than the active display.
 #
 # To use this script with wpgtk, you will need to prevent it from setting
 # wallpapers and you will need to configure it to run a command after a
@@ -11,16 +10,15 @@
 # - In the "Command" box, enter "bash /path/to/tile-wallpaper.sh".
 #
 # To restore wallpapers on startup, add tile-wallpaper.sh to the autostart
-# of your window manager after initializing wpgtk. I achieve the latter by 
-# using the "wp-init.sh" file in wpg's "~/.config" directory in my xinitrc.
-# At the moment, I'm not sure of a better way to do this.
+# of your window manager after initializing wpgtk.
 
 # Monitor width
-w=1920
+w=$(xrandr | fgrep '*' | awk '{print $1}' | sed -e 's/x/ /g' | cut -f1 -d' ')
 
 # Monitor height
-h=1080
+h=$(xrandr | fgrep '*' | awk '{print $1}' | sed -e 's/x/ /g' | cut -f2 -d' ')
 
+# Compares width and height of cached wallpaper to monitor resolution
 if (( $(identify -format "%w" "$(< "${HOME}/.cache/wal/wal")") < $w )) && (( $(identify -format "%h" "$(< "${HOME}/.cache/wal/wal")") < $h ))
 then
 	feh --bg-tile "$(< "${HOME}/.cache/wal/wal")"
