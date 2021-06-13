13e629c128a31957371876d6ae5eda18782db141
Graham <47785671+808-cowbell@users.noreply.github.com>
2021-06-13 12:16:23 -0600

    Delete Shell Scripts directory

--- Shell Scripts/lock.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-# This script provides a locking mechanism for laptops. Call it from
-# anywhere you prefer, e.g. .xinitrc or .bspwmrc.
-
-set -e
-xset s off dpms 0 10 0
-i3lock --color=$(sed 's/#//; 1q' .cache/wal/colors) --ignore-empty-password --show-failed-attempts --nofork
-xset s off dpms 600 600 600
